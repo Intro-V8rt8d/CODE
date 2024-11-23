829eb4e0826995280799dc853bd5ca2855efc4dc
INTROV8RT8D <adewumie000@gmil.com>
2024-11-23 17:51:22 +0100

    fe done

--- bootstrap-bootcamp-website-main/template.js
+++ bootstrap-bootcamp-website-main/template.js
@@ -185,3 +185,23 @@ if (response.ok) {
 
 // Call the function
 deleteUser(1);
+
+//TRY AND CATCH AND THROW
+function myFunction() {
+    const message = document.getElementById("p01");
+    message.innerHTML = "";
+    let x = document.getElementById("demo").value;
+    try {
+      if(x.trim() == "") throw "is empty";
+      if(isNaN(x)) throw "is not a number";
+      x = Number(x);
+      if(x > 10) throw "is too high";
+      if(x < 5) throw "is too low";
+    }
+    catch(err) {
+      message.innerHTML = "Error: " + err + ".";
+    }
+    finally {
+      document.getElementById("demo").value = "";
+    }
+  }
